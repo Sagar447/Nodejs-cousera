c0b56ad1e8a8775e263e42bdafcb16ef5272b3da
Sagar Naik <sagarnaik447@gmail.com>
2020-11-22 20:43:43 +0530

    simple node module

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var rec= {
-  perimeter: (x, y) => (2*(x+y)),
-  area : (x, y) => (x*y)
-};
+var rec= require("./rectangle");
  function solveRec(l,b){
    console.log("Solving the rectangle with l ="+l+" and b ="+b);
    if(l<=0 || b<=0){
