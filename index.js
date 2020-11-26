72e2c8879dc68875c476b126ff771d30e22c2f0d
Sagar Naik <sagarnaik447@gmail.com>
2020-11-26 21:23:02 +0530

    error and callbacks

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var rec= require("./rectangle");
+var rect= require("./rectangle");
  function solveRec(l,b){
    console.log("Solving the rectangle with l ="+l+" and b ="+b);
-   if(l<=0 || b<=0){
-     console.log("The l and b is must be greater than 0");
-   }else{
-     console.log("The perimeter is "+rec.perimeter(l,b));
-     console.log("The area is "+rec.area(l,b));
-   }
+   rect(l, b, (err, rectangle) =>{
+    if(err){
+          console.log("ERROR", err.message);
+    }else{
+      console.log("the area of the rectangle is "+rectangle.area());
+      console.log("the perimeter of the rectangle is "+rectangle.perimeter());
+    }
+   });
+   console.log("this statement is after the call to rect()");
  }
  solveRec(5, 4);
  solveRec(4, 1);
